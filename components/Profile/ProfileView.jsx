cdd557ee2200df7e0d84d029c4770ae32898c8bd
shivamgulia <shivam.meert@gmail.com>
2024-11-08 09:21:24 +0530

    user update

--- components/Profile/ProfileView.jsx
+++ components/Profile/ProfileView.jsx
@@ -24,16 +24,16 @@ function ProfileView({ user }) {
       <h2 className={styles.profileTitle}>Profile Information</h2>
       <div className={styles.profileInfo}>
         <div className={styles.profileItem}>
-          <span className={styles.label}>Name:</span> {user.name}
+          <span className={styles.label}>Name:</span> {user?.name}
         </div>
         <div className={styles.profileItem}>
-          <span className={styles.label}>Address:</span> {user.address}
+          <span className={styles.label}>Address:</span> {user?.address}
         </div>
         <div className={styles.profileItem}>
-          <span className={styles.label}>Contact:</span> {user.contact}
+          <span className={styles.label}>Contact:</span> {user?.contact}
         </div>
         <div className={styles.profileItem}>
-          <span className={styles.label}>Email:</span> {user.email}
+          <span className={styles.label}>Email:</span> {user?.email}
         </div>
       </div>
       <button onClick={editProfile}>Edit Profile</button>
